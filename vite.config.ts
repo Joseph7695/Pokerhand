d71038dc79f89f014b750dc35f0f0c3c0de4a50d
Joseph7695 <36186848+Joseph7695@users.noreply.github.com>
2025-06-10 00:40:10 +0800

    jebaited with the baseurl ?

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Pokerhand/',
   plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
